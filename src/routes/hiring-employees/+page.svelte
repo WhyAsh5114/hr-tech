3a652f70fad3d57d409a9ce2e201aa12cbf4f322
WhyAsh5114 <whyash5114@gmail.com>
2024-03-16 14:45:28 +0530

    added inter font and heading

--- src/routes/hiring-employees/+page.svelte
+++ src/routes/hiring-employees/+page.svelte
@@ -1,6 +1,5 @@
-
-<script>
-    import pdf from "pdf-parse"
-    
-
-</script>
+<h2
+  class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 mb-6"
+>
+  Hiring employees
+</h2>
