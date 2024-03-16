b569da80cbda1ba369b1e2b18c69ff74078519d1
Your Name here only <krishnsundaram@gmail.com>
2024-03-16 21:28:44 +0530

    adding email attributes in data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,30 +3,36 @@ export const employeeData = [
     name: "John Doe",
     username: "samarthsb4real",
     designation: "Senior Software Developer",
+    email: "yashkolekar1234@gmail.com",
   },
   {
     name: "Albert King",
     username: "WhyAsh5114",
     designation: "Cloud Architect",
+    email: "anonjr192@gmail.com",
   },
   {
     name: "Jessica Jones",
     username: "sundaram123krishnan",
     designation: "System Administrator",
+    email: "anonjr511@gmail.com",
   },
   {
     name: "Jashan Elizabeth",
     username: "GodBoii",
     designation: "Junior Developer",
+    email: "myfit.auth@gmail.com",
   },
   {
     name: "Shayla Nico",
     username: "lowercasexyz",
     designation: "Senior Software Developer",
+    email: "whyashplaysdota@gmail.com",
   },
   {
     name: "Ray Liotta",
     username: "Kushhhh-sh",
     designation: "Database Administrator",
+    email: "krishnsundaram@gmail.com",
   },
 ];
