fae8b851b7d396cd53326c508aa69b0c8ca11197
WhyAsh5114 <whyash5114@gmail.com>
2024-03-17 06:48:57 +0530

    added sorting and search

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,35 +4,47 @@ export const employeeData = [
     username: "samarthsb4real",
     designation: "Senior Software Developer",
     email: "yashkolekar1234@gmail.com",
+    issuesClosed: 5,
+    commits: 15
   },
   {
     name: "Albert King",
     username: "WhyAsh5114",
     designation: "Cloud Architect",
     email: "anonjr192@gmail.com",
+    issuesClosed: 5,
+    commits: 25
   },
   {
     name: "Jessica Jones",
     username: "sundaram123krishnan",
     designation: "System Administrator",
     email: "anonjr511@gmail.com",
+    issuesClosed: 7,
+    commits: 22
   },
   {
     name: "Jashan Elizabeth",
     username: "GodBoii",
     designation: "Junior Developer",
     email: "myfit.auth@gmail.com",
+    issuesClosed: 2,
+    commits: 12
   },
   {
     name: "Shayla Nico",
     username: "lowercasexyz",
     designation: "Senior Software Developer",
     email: "whyashplaysdota@gmail.com",
+    issuesClosed: 3,
+    commits: 52
   },
   {
     name: "Ray Liotta",
     username: "Kushhhh-sh",
     designation: "Database Administrator",
     email: "krishnsundaram@gmail.com",
+    issuesClosed: 9,
+    commits: 18
   },
 ];
--- src/routes/manage-employees/+page.svelte
+++ src/routes/manage-employees/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import {
     Card,
     CardContent,
@@ -12,12 +12,58 @@
     AvatarFallback,
     AvatarImage,
   } from "$lib/components/ui/avatar";
+  import { Input } from "$lib/components/ui/input";
+  import * as Select from "$lib/components/ui/select";
 
   export let data;
+  let searchText = "";
+  let employeeData = data.employeeData;
+  const sorters = [
+    { value: "issues closed", label: "Issues Closed" },
+    { value: "commits", label: "Commits" },
+  ];
+  let selectedSorter: undefined | { value: string; label: string } = undefined;
+
+  $: employeeData = data.employeeData.filter((employee) =>
+    employee.name.toLowerCase().includes(searchText)
+  );
+
+  $: employeeData = employeeData.sort((b, a) => {
+    console.log(selectedSorter?.value);
+    if (selectedSorter?.value === "issues closed") {
+      return a.issuesClosed - b.issuesClosed;
+    } else if (selectedSorter?.value === "commits") {
+      return a.commits - b.commits;
+    } else {
+      return Number(a.name) - Number(b.name);
+    }
+  });
 </script>
 
+<div class="flex gap-2">
+  <Input
+    class="mb-4"
+    placeholder="Search for employee"
+    bind:value={searchText}
+  />
+  <Select.Root bind:selected={selectedSorter}>
+    <Select.Trigger class="basis-1/3">
+      <Select.Value placeholder="Sort by" />
+    </Select.Trigger>
+    <Select.Content>
+      <Select.Group>
+        {#each sorters as sorter}
+          <Select.Item value={sorter.value} label={sorter.label}
+            >{sorter.label}</Select.Item
+          >
+        {/each}
+      </Select.Group>
+    </Select.Content>
+    <Select.Input name="favoriteFruit" />
+  </Select.Root>
+</div>
 <div class="grid grid-cols-3 gap-4">
-  {#each data.employeeData as employee}
+  {#each employeeData as employee}
     <a href="/manage-employees/{employee.name}">
       <Card
         class="flex items-center justify-start px-6  hover:shadow-md transition-all"
@@ -30,6 +76,13 @@
           <CardTitle>{employee.name}</CardTitle>
           <CardDescription>{employee.designation}</CardDescription>
         </CardHeader>
+        <CardDescription class="ml-auto font-semibold text-lg text-foreground">
+          {#if selectedSorter?.value === "issues closed"}
+            <span>{employee.issuesClosed}</span>
+          {:else if selectedSorter?.value === "commits"}
+            <span>{employee.commits}</span>
+          {/if}
+        </CardDescription>
       </Card>
     </a>
   {/each}
